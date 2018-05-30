cc52f0737c3050dc23c156dfd7ec5fe252619867
Joe Ortega <joeortega79@gmail.com>
2018-05-30 16:59:42 +0930

    * ListsDataSource.cs: 0.001.003
    * Main.storyboard:
    * Groceries.iOS.csproj:
    * ListViewController.cs:

---
+++ iOS/Classes/ListsDataSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Foundation;
+using UIKit;
+namespace Groceries.iOS
+{
+    public class ListsDataSource : UITableViewSource
+    {
+        readonly UIViewController dataSourceController;
+
+        public ListsDataSource (UIViewController inpCtrl)
+        {
+            dataSourceController = inpCtrl;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return 5;
+        }
+   
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell("listsCell");
+
+            cell.TextLabel.Text = "Test";
+
+            return cell;
+        }
+    }
+}
--- iOS/ListViewController.cs
+++ iOS/ListViewController.cs
@@ -13,6 +13,9 @@ namespace Groceries.iOS
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
+            ListsDataSource tableDs = new ListsDataSource(this);
+            groceryListTableView.Source = tableDs;
         }
 
         partial void NewListButton_TouchUpInside(UIButton sender)
