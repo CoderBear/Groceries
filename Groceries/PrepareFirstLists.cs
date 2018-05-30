ff7e5204fc189b5fbce587ce5fbf7e3c0b7fc0f6
Joe Ortega <joeortega79@gmail.com>
2018-05-30 20:44:45 +0930

    0.001.007

---
+++ Groceries/PrepareFirstLists.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Groceries
+{
+    public static class PrepareFirstLists
+    {
+        public static void Prepare()
+        {
+            AppData.currentLists = new List<GroceryListClass>();
+
+            AppData.currentLists.Add(new GroceryListClass()
+            {
+                Name = "Sample List",
+                Owner = AppData.curUser,
+                Items = new List<ItemClass>()
+            });
+        }
+    }
+}
